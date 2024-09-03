70c4e5f44d6d05428b726a1b35571a84e1957eec
Elysia125 <feng89310@gmail.com>
2024-09-03 09:05:16 +0800

    second

---
+++ src/main/java/org/example/tabletopgames/Login.java
@@ -0,0 +1,8 @@
+package org.example.tabletopgames;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Login{
+    
+}
---
+++ src/main/java/org/example/tabletopgames/Register.java
@@ -0,0 +1,7 @@
+package org.example.tabletopgames;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class Register {
+
+}
